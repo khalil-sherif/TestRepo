835f6f57a33dbf4c78c0937bd94df6c21231a216
khalil-sherif <khalil.sherif@crescendotechnology.com>
2017-03-21 14:36:43 -0400

    Creating a merge conflict

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 var express = require("express")
 var config = require("./config.json")
-var app = express()
-
-app.get("/", function(req, res){
-	res.send("Test")
-})
 
 app.listen(config.express.port, function(){
 	console.log("Listening on port " + config.express.port)
